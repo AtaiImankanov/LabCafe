874ee919a4b966784db6edfa57efb93ae568bcd1
atai <imankanov2002@gmail.com>
2022-11-04 14:58:29 +0600

    Issue#2 done

--- Startup.cs
+++ Startup.cs
@@ -97,7 +97,7 @@ namespace homework_64_Atai
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Cafe}/{action=Index}/{id?}");
             });
         }
     }
